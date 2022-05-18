7aef62e78d6062f5311b924228b627b0e684d834
Pavantoleti <97424427+Pavantoleti@users.noreply.github.com>
2022-05-18 23:57:30 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int i,j;     for(i=0;i<n;i++)     {         for(j=0;j<i+1;j++)         {             printf("%d",j+1);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -6,11 +6,11 @@ int main()
     int i,j;
     for(i=0;i<n;i++)
     {
-        for(j=0;j<n;j++)
+        for(j=0;j<i+1;j++)
         {
-            printf("%d ",n-j);
-       }
-       printf("
+            printf("%d",j+1);
+        }
+        printf("
 ");
     }
 }
