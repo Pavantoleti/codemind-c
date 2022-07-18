e618b4830fad14642353d4cc9b1b727a5c5de49a
Pavantoleti <97424427+Pavantoleti@users.noreply.github.com>
2022-07-18 22:06:22 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     int arr[a],i;     for (i=0;i<a;i++)     {         scanf("%d",&arr[i]);     }     int index=0;     for (i=0;i<a;i++)     {         if (arr[i]%2!=0)         {             index=i;         }     }     printf("%d",index);     return 0; }

--- last_odd_number_index_position.c
+++ last_odd_number_index_position.c
@@ -4,12 +4,12 @@ int main()
     int a;
     scanf("%d",&a);
     int arr[a],i;
-    for (i=0; i<a; i++)
+    for (i=0;i<a;i++)
     {
         scanf("%d",&arr[i]);
     }
     int index=0;
-    for (i=0; i<a; i++)
+    for (i=0;i<a;i++)
     {
         if (arr[i]%2!=0)
         {
