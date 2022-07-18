4e65101eeab90aaae077c0f4b56fb459bc61cf7b
Pavantoleti <97424427+Pavantoleti@users.noreply.github.com>
2022-07-18 21:36:23 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n];     int sum=0;     for(int i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(int i=0;i<n;i++)     {         if(i%2==0)         {             sum+=arr[i];         }     }     printf("%d",sum); }

--- Sum_of_Even_Positions.c
+++ Sum_of_Even_Positions.c
@@ -4,11 +4,11 @@ int main()
     int n;
     scanf("%d",&n);
     int arr[n];
+    int sum=0;
     for(int i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
     }
-    int sum=0;
     for(int i=0;i<n;i++)
     {
         if(i%2==0)
